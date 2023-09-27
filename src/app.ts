4a92f0a3ff28755a6a85ac6f28877eded33a9524
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2023-09-27 21:19:38 +0200

    add http errors package, adjust error status code and messages

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import recipesRoutes from "./routes/recipesRouter";
 import morgan from "morgan";
+import createHttpError, { isHttpError } from "http-errors";
 
 const app = express();
 
@@ -11,13 +12,18 @@ app.use(express.json());
 app.use("/api/recipes", recipesRoutes);
 
 app.use((req, res, next) => {
-  next(Error("Endpoint not found"));
+  next(createHttpError(404, "Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
   let errorMessage = "An unexpected error occurred.";
-  if (error instanceof Error) errorMessage = error.message;
-  res.status(500).json({ error: errorMessage });
+  let statusCode = 500;
+
+  if (isHttpError(error)) {
+    errorMessage = error.message;
+    statusCode = error.status;
+  }
+  res.status(statusCode).json({ error: errorMessage });
 });
 export default app;
--- src/controller/recipes.ts
+++ src/controller/recipes.ts
@@ -1,11 +1,22 @@
 import { RequestHandler } from "express";
 import RecipeModel from "../model/recipe";
+import mongoose from "mongoose";
+import createHttpError from "http-errors";
 
 export const getRecipe: RequestHandler = async (req, res, next) => {
   const recipeId = req.params.recipeId;
 
   try {
+    if (!mongoose.isValidObjectId(recipeId)) {
+      throw createHttpError(400, "Invalid recipe id.");
+    }
+
     const recipe = await RecipeModel.findById(recipeId).exec();
+
+    if (!recipe) {
+      throw createHttpError(404, "No recipe id found.");
+    }
+
     res.status(200).json(recipe);
   } catch (error) {
     next(error);
@@ -21,13 +32,29 @@ export const getRecipes: RequestHandler = async (req, res, next) => {
   }
 };
 
-export const createRecipe: RequestHandler = async (req, res, next) => {
+interface CreateRecipeBody {
+  title?: string;
+  levelOfDifficulty?: number;
+  preparationTime?: number;
+  ingredients?: string[];
+}
+
+export const createRecipe: RequestHandler<
+  unknown,
+  unknown,
+  CreateRecipeBody,
+  unknown
+> = async (req, res, next) => {
   const title = req.body.title;
   const levelOfDifficulty = req.body.levelOfDifficulty;
   const preparationTime = req.body.preparationTime;
   const ingredients = req.body.ingredients;
 
   try {
+    if (!title) {
+      throw createHttpError(400, "The recipe needs a title.");
+    }
+
     const newRecipe = await RecipeModel.create({
       title,
       levelOfDifficulty,
