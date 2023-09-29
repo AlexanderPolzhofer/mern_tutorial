a20127af1e93cb169706b6cf7de4b9bfe006cc28
AlexanderPolzhofer <alexander.polzhofer@gmail.com>
2023-09-29 07:24:55 +0200

    use loadRecipes method to fetch data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,14 @@
 import React from "react";
-import { Recipe } from "./model/recipe";
+import { Recipe as RecipeModel } from "./model/recipe";
+import { loadRecipes } from "./util/loadRecipes";
+
 
 const App = () => {
-  const [recipes, setRecipes] = React.useState<Recipe[]>([]);
+  const [recipes, setRecipes] = React.useState<RecipeModel[]>([]);
   const endpoint = "/api/recipes";
 
   React.useEffect(() => {
-    const loadRecipes = async () => {
-      try {
-        const response = await fetch(endpoint, { method: "GET" });
-        const fetchedRecipes: Recipe[] = await response.json();
-        setRecipes(fetchedRecipes);
-      } catch (error) {
-        console.error();
-      }
-    };
-    loadRecipes();
+    loadRecipes(endpoint, "GET", setRecipes);
   }, []);
 
   return <>{JSON.stringify(recipes)}</>;
--- frontend/src/util/loadRecipes.ts
+++ frontend/src/util/loadRecipes.ts
@@ -1,13 +1,13 @@
-import { Recipe } from "../model/recipe";
+import { Recipe as RecipeModel } from "../model/recipe";
 
 export const loadRecipes = async (
   host: string,
-  method: string,
-  setState: React.Dispatch<React.SetStateAction<Recipe[]>>
+  requestMethod: string,
+  setState: React.Dispatch<React.SetStateAction<RecipeModel[]>>
 ) => {
   try {
-    const response = await fetch(host, {method: method});
-    const data: Recipe[] = await response.json();
+    const response = await fetch(host, { method: requestMethod });
+    const data: RecipeModel[] = await response.json();
     setState(data);
   } catch (error) {
     console.error(error);
